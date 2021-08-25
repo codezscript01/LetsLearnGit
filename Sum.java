543b732f48fe482aa20791543c37e6d6d7e363a4
Shruti Singhal <shruti1110singhal@gmail.com>
2021-08-25 16:44:16 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class Sum
+{
+    public static void main(String args[])
+    {
+        int a=5,b=10;
+        System.out.println(a+b);
+    }
+
+}
